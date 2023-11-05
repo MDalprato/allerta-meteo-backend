3902a4304fbd8f49da033d7bcd4aeea1577f92bd
MDalprato <marcodalprato@me.com>
2023-11-05 15:38:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time?variabile=254,0,0/1,-,-,-/B13215&time=1699191000000
+
+const https = require('https');
+
+const uri = 'https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time?variabile=254,0,0/1,-,-,-/B13215&time=1699191000000';
+
+https.get(uri, (response) => {
+  let data = '';
+
+  response.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  response.on('end', () => {
+    // Parse the JSON response
+    const jsonData = JSON.parse(data);
+
+    // Find the station with the name "Castelbolognese"
+    const stationName = "Castelbolognese";
+    const stationData = jsonData.find(station => station.nomestaz === stationName);
+
+    if (stationData) {
+      // Extract the "value" property from the station data
+      const value = stationData.value;
+      console.log(`Value for station ${stationName}: ${value}`);
+    } else {
+      console.log(`Station "${stationName}" not found in the data.`);
+    }
+  });
+}).on('error', (error) => {
+  console.error(`Error: ${error.message}`);
+});
