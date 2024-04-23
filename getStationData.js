4cd3ff766c7219a06347f72b1640089c0cecc858
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2024-04-23 21:00:36 +0200

    Trovata la logica con cui viene ottentuo il timestamp

--- getStationData.js
+++ getStationData.js
@@ -2,12 +2,33 @@
 
 const https = require('https');
 var asciichart = require('asciichart');
+const moment = require('moment');
 
+function getTimestamp(){
+
+    var d = new Date
+    d.setMilliseconds(0)
+    d.setSeconds(0)
+    d.setMinutes(d.getMinutes()<31?0:30)
+    
+    // Tira indietro di 2 ore perché nel db sono UTC
+    if (moment(d).isDST())
+        d.setHours(d.getHours() - 2); // Solare
+    else
+        d.setHours(d.getHours() - 1); // Legale
+
+    ts = +d
+    return ts; 
+};
 
 function getDataFromStream(stationNamesToRetrieve) {
     return new Promise((resolve, reject) => {
-        const uri = 'https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time?variabile=254,0,0/1,-,-,-/B13215&time=1699191000000';
 
+        const ts = getTimestamp();
+
+        const uri = `https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time?variabile=254,0,0/1,-,-,-/B13215&time=${ts}`
+
+        
         https.get(uri, (response) => {
             let data = '';
 
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const generateChart = require("./generateChart");
-const getFromDb = require("./getFromDb");
+
 const getDataFromStream = require("./getStationData");
-const stationsData = require('./rivers/rivers.json');
-const saveToDb = require("./saveToDb");
 
+const data = [
+    "Marradi",
+    "Strada Casale",
+    "Sarna",
+    "Faenza",
+    "Reda",
+    "Pieve Cesato",
+    "Mezzano"
+];
 
+getDataFromStream(data);
