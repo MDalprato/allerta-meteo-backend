518da06433346efbacb90857c1c9b8e07fc24dcc
MDalprato <marcodalprato@me.com>
2024-05-09 17:16:29 +0200

    having fun !

--- config.js
+++ config.js
@@ -3,5 +3,4 @@ module.exports = {
     dbConnection : "mongodb://localhost:27017/allerta_meteo",
     get_sensor_values_url : `https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time`,
     get_sensor_values_type : '254,0,0/1,-,-,-/B13215',
-
 };
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const http = require('http');
 const mongoose = require('mongoose');
 const { dbConnection } = require('./config');
 
-// Connessione al database MongoDB tramite Mongoose
 mongoose.connect(dbConnection)
     .then(() => {
         console.log("Successefully connected to MongoDB");
@@ -11,39 +10,30 @@ mongoose.connect(dbConnection)
         console.error("Errore durante la connessione al database MongoDB:", error);
     });
 
-// Modello flessibile per la collezione "stations"
 const Station = mongoose.model('Station', {}); // Non specifica uno schema
 
-// Crea il server
 const server = http.createServer(async (req, res) => {
-    // Verifica se la richiesta è per la rotta /stazioni
     if (req.url === '/stazioni') {
         try {
-            // Trova tutti i documenti nella collezione "stations"
             const stations = await Station.find().lean(); // Utilizza .lean() per ottenere oggetti JavaScript normali
-
-            // Imposta lo status della risposta a 200 (OK)
-            res.writeHead(200, {'Content-Type': 'application/json'});
-
-            // Invia i dati delle stazioni come JSON
+            res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(stations));
         } catch (error) {
-            // Se si verifica un errore, rispondi con un 500 (Internal Server Error)
             console.error("Errore durante la richiesta delle stazioni:", error);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Errore durante la richiesta delle stazioni\n');
         }
     } else {
-        // Se la rotta richiesta non è /stazioni, rispondi con un 404 (Not Found)
-        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404 Not Found\n');
     }
 });
 
-// Definisci la porta su cui il server ascolterà le richieste
 const PORT = process.env.PORT || 3000;
 
-// Avvia il server e fai in modo che ascolti sulla porta specificata
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log("┌─┐┌─┐┬─┐┬  ┬┌─┐┬─┐  ┬┌─┐  ┬─┐┬ ┬┌┐┌┌┐┌┬┌┐┌┌─┐ ");
+    console.log("└─┐├┤ ├┬┘└┐┌┘├┤ ├┬┘  │└─┐  ├┬┘│ │││││││││││ ┬  ");
+    console.log("└─┘└─┘┴└─ └┘ └─┘┴└─  ┴└─┘  ┴└─└─┘┘└┘┘└┘┴┘└┘└─┘ ");
 });
