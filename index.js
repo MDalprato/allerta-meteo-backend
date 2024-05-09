10af1f7902e26e3f31188f9baf2c6aa8096ec163
MDalprato <marcodalprato@me.com>
2024-05-09 17:11:36 +0200

    Aggiunta rotta /stazioni

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-// Importa il modulo http di Node.js
 const http = require('http');
+const mongoose = require('mongoose');
+const { dbConnection } = require('./config');
+
+// Connessione al database MongoDB tramite Mongoose
+mongoose.connect(dbConnection)
+    .then(() => {
+        console.log("Successefully connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Errore durante la connessione al database MongoDB:", error);
+    });
+
+// Modello flessibile per la collezione "stations"
+const Station = mongoose.model('Station', {}); // Non specifica uno schema
 
 // Crea il server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     // Verifica se la richiesta è per la rotta /stazioni
     if (req.url === '/stazioni') {
-        // Imposta lo status della risposta a 200 (OK)
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        try {
+            // Trova tutti i documenti nella collezione "stations"
+            const stations = await Station.find().lean(); // Utilizza .lean() per ottenere oggetti JavaScript normali
 
-        // Invia la risposta "Hello World"
-        res.end('Hello World\n');
+            // Imposta lo status della risposta a 200 (OK)
+            res.writeHead(200, {'Content-Type': 'application/json'});
+
+            // Invia i dati delle stazioni come JSON
+            res.end(JSON.stringify(stations));
+        } catch (error) {
+            // Se si verifica un errore, rispondi con un 500 (Internal Server Error)
+            console.error("Errore durante la richiesta delle stazioni:", error);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Errore durante la richiesta delle stazioni\n');
+        }
     } else {
         // Se la rotta richiesta non è /stazioni, rispondi con un 404 (Not Found)
         res.writeHead(404, {'Content-Type': 'text/plain'});
