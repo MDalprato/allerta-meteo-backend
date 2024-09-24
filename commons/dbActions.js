cccfe48b34fb8917e6b5e599ac3f6058065a5ec4
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2024-09-24 21:20:46 +0200

    fix

--- commons/dbActions.js
+++ commons/dbActions.js
@@ -3,10 +3,12 @@ const StationModel = require('../Schemas/Station');
 const AlertModel = require('../Schemas/Alert');
 require('dotenv').config();
 
-const dbConnection = process.env.DB_CONNECTION || 'mongodb://localhost:27017/allerta_meteo';
+const dbConnection = process.env.DB;
+console.log('Saving stations to db: ', dbConnection);
+
+
 async function saveStationsToDb(stationsToBeSaved) {
 
-  console.log('Saving stations to db: ', dbConnection);
 
   if (!Array.isArray(stationsToBeSaved) || stationsToBeSaved.length === 0) {
     console.log("Invalid stations to be saved");
